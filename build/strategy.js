98d9eb5d3eaba48c93c8e61fea03b8676a6d3cc6
satiseven <satiseven777@gmail.com>
2023-12-19 16:59:56 +0300

    strategy pattern

--- build/strategy.js
+++ build/strategy.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 - payment system that supports PAYPAL and STRIPE
 
@@ -5,58 +6,40 @@
 - when a user pays with PAYPAL, they will be redirected to PayPal's website where they can enter their details (
 
 */
-
- 
-
-// Interface for payment methods
-interface PaymentMethod {
-    pay(amount: number): void;
-}
-
 // Concrete strategy for PayPal
-class PayPal implements PaymentMethod {
-    pay(amount: number): void {
+class PayPal {
+    pay(amount) {
         console.log(`Paid $${amount} via PayPal.`);
         // Additional logic for redirecting to PayPal's website
     }
 }
-
 // Concrete strategy for Stripe
-class Stripe implements PaymentMethod {
-    pay(amount: number): void {
+class Stripe {
+    pay(amount) {
         console.log(`Paid $${amount} via Stripe.`);
         // Additional logic for processing payment with Stripe
     }
 }
-
 // Context class that uses the payment strategy
 class Payment {
-    private paymentMethod: PaymentMethod;
-
-    constructor(paymentMethod: PaymentMethod) {
+    constructor(paymentMethod) {
         this.paymentMethod = paymentMethod;
     }
-
     // Setter method to change the payment strategy dynamically
-    setPaymentMethod(paymentMethod: PaymentMethod): void {
+    setPaymentMethod(paymentMethod) {
         this.paymentMethod = paymentMethod;
     }
-
     // Method to perform the payment using the selected strategy
-    performPayment(amount: number): void {
+    performPayment(amount) {
         this.paymentMethod.pay(amount);
     }
 }
-
 // Client code
 const paypalPayment = new PayPal();
 const stripePayment = new Stripe();
-
 const payment = new Payment(paypalPayment);
-
 // Performing payment using the current strategy (PayPal)
 payment.performPayment(100);
-
 // Switching to a different payment strategy (Stripe)
 payment.setPaymentMethod(stripePayment);
 payment.performPayment(50);
