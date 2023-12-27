078ea13703b4d23e6e6ce2aee76e055b3b2a6d5b
satiseven <satiseven777@gmail.com>
2023-12-27 14:38:55 +0300

    observer pattern

---
+++ build/observer.js
@@ -0,0 +1,40 @@
+"use strict";
+// Concrete Observer
+class ConcreteObserver {
+    constructor(name) {
+        this.name = name;
+    }
+    update(data) {
+        console.log(`${this.name} received update with data: ${data}`);
+    }
+}
+// Concrete Subject
+class ConcreteSubject {
+    constructor() {
+        this.observers = [];
+    }
+    // Method to add observers
+    addObserver(observer) {
+        this.observers.push(observer);
+    }
+    // Method to remove observers
+    removeObserver(observer) {
+        this.observers = this.observers.filter(obs => obs !== observer);
+    }
+    // Method to notify observers of a change
+    notifyObservers(data) {
+        this.observers.forEach(observer => observer.update(data));
+    }
+}
+// Example usage
+const subject = new ConcreteSubject();
+const observer1 = new ConcreteObserver("Observer 1");
+const observer2 = new ConcreteObserver("Observer 2");
+// Adding observers to the subject
+subject.addObserver(observer1);
+subject.addObserver(observer2);
+// Notifying observers of a change
+subject.notifyObservers("Hello, observers!");
+// Output:
+// Observer 1 received update with data: Hello, observers!
+// Observer 2 received update with data: Hello, observers!
---
+++ observer.ts
@@ -0,0 +1,62 @@
+// Observer interface
+interface Observer {
+    update(data: any): void;
+  }
+  
+  // Subject interface
+  interface Subject {
+    addObserver(observer: Observer): void;
+    removeObserver(observer: Observer): void;
+    notifyObservers(data: any): void;
+  }
+  
+  // Concrete Observer
+  class ConcreteObserver implements Observer {
+    private name: string;
+  
+    constructor(name: string) {
+      this.name = name;
+    }
+  
+    update(data: any): void {
+      console.log(`${this.name} received update with data: ${data}`);
+    }
+  }
+  
+  // Concrete Subject
+  class ConcreteSubject implements Subject {
+    private observers: Observer[] = [];
+  
+    // Method to add observers
+    addObserver(observer: Observer): void {
+      this.observers.push(observer);
+    }
+  
+    // Method to remove observers
+    removeObserver(observer: Observer): void {
+      this.observers = this.observers.filter(obs => obs !== observer);
+    }
+  
+    // Method to notify observers of a change
+    notifyObservers(data: any): void {
+      this.observers.forEach(observer => observer.update(data));
+    }
+  }
+  
+  // Example usage
+  const subject: Subject = new ConcreteSubject();
+  
+  const observer1 = new ConcreteObserver("Observer 1");
+  const observer2 = new ConcreteObserver("Observer 2");
+  
+  // Adding observers to the subject
+  subject.addObserver(observer1);
+  subject.addObserver(observer2);
+  
+  // Notifying observers of a change
+  subject.notifyObservers("Hello, observers!");
+  
+  // Output:
+  // Observer 1 received update with data: Hello, observers!
+  // Observer 2 received update with data: Hello, observers!
+  
