e3e67a641f28571b80a7f5de26a48c1010b08983
satiseven <satiseven777@gmail.com>
2024-01-08 09:01:55 +0300

    singleton pattern added

---
+++ singleton.ts
@@ -0,0 +1,27 @@
+class Singleton {
+    private static singletonInstance: Singleton | null = null;
+
+    private constructor() {
+        // Initialization logic, if needed
+        console.log("class created");
+        
+    }
+
+    static singletonCreator(): Singleton {
+        if (this.singletonInstance === null) {
+            this.singletonInstance = new Singleton();
+        }
+        return this.singletonInstance;
+    }
+    sumTwoNumbers(numOne:number,numTow:number):number{
+        return numOne + numTow;
+    }
+}
+
+const singleton= Singleton.singletonCreator();
+console.log(singleton.sumTwoNumbers(5,6));
+
+const singleton2= Singleton.singletonCreator();
+
+console.log(singleton2.sumTwoNumbers(15,2));
+;
